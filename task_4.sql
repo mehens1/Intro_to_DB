3411ffbf07ee980a81709b9b056b9b17142c49bd
Meheni Samson <54425905+mehens1@users.noreply.github.com>
2024-07-28 17:31:13 +0100

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,13 +1,7 @@
--- -- Use the specified database
--- USE alx_book_store;
-
--- -- Print the full description of the table books
--- SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
--- FROM information_schema.COLUMNS
--- WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
-
 -- Use the specified database
 USE alx_book_store;
 
 -- Print the full description of the table books
-SHOW CREATE TABLE Books;
+SELECT TABLE_NAME, COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA 
+FROM information_schema.COLUMNS
+WHERE TABLE_NAME = 'Books' AND TABLE_SCHEMA = 'alx_book_store';
